c8a5f3d76d4736d5386d0935ca23c95f3dd7f0a3
Gmashil027098 <Gmashil027098@gmail.com>
2019-03-30 17:55:40 +0500

    Assignment ex24

---
+++ ForVar.java
@@ -0,0 +1,13 @@
+//Declare loop control variable inside the for.
+class ForVar {
+	public static void main(String srgs[]){
+		int sum=0;
+		int fact =1;
+		for (int i=1; i<=5;i++){
+			sum +=i;
+			fact *= i;
+		}
+		System.out.println("Sum is : "+sum);
+		System.out.println("Factorial is : "+fact);
+	}
+}
