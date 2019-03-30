ff016b396be9424656986fc2dd6bf0e15ace6b5e
Gmashil027098 <Gmashil027098@gmail.com>
2019-03-30 16:47:41 +0500

    Assignment ex19

---
+++ Q7.java
@@ -0,0 +1,14 @@
+//
+class Q7{
+	public static void main(String args[]){
+	int x = 5 ,y = 8;
+	for (int i = 0; i <10 ; i++ ) {
+		while(running){
+			if(x<y)break;
+			// ...
+		}
+		System.out.println("After while");
+	}
+	System.out.println("After for");
+	}
+	}
