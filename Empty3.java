55682d6be08cb90656be88a2ec488749a2e4a35a
Gmashil027098 <Gmashil027098@gmail.com>
2019-03-30 16:43:19 +0500

    Assignment ex6

---
+++ Empty3.java
@@ -0,0 +1,9 @@
+//Parts of the for can be empty.
+class Empty3{
+	public static void main (String args[]){
+		int i,sum=0;
+		for (i=0;i<=5;sum+=i++);
+		
+		System.out.println("Sum is "+sum);		
+	}
+}
