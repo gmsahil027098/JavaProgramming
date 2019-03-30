9a5801adc16699deebee5881e6de6551e6205372
Gmashil027098 <Gmashil027098@gmail.com>
2019-03-30 16:49:11 +0500

    Assignment ex24

---
+++ WhileDemo.java
@@ -0,0 +1,11 @@
+//Demonstration of while loop.
+class WhileDemo{
+	public static void main(String args[]){
+		char ch;
+		ch ='a';
+		while(ch<='z'){
+			System.out.print(ch);
+			ch++;
+		}
+	}
+}
