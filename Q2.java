a931684cd1ee4cc065a1a5b3f32a8dbf22cbb0c9
Gmashil027098 <Gmashil027098@gmail.com>
2019-03-30 16:47:27 +0500

    Assignment ex18

---
+++ Q2.java
@@ -0,0 +1,17 @@
+//General Form of If-else=If ladder.
+class Q2{
+	public static void main(String args[]){
+		System.out.println("\nThis is only general form of If Else If ladder No Input No Output\n");
+		System.out.println("if(condition)");
+		System.out.println("	Statement;");
+		System.out.println("else if(condition)");
+		System.out.println("	Statement;");
+		System.out.println("else if(condition)");
+		System.out.println("	Statement;");
+		System.out.println("	.");
+		System.out.println("	.");
+		System.out.println("	.");
+		System.out.println("else ");
+		System.out.println("	Statement;");
+	}
+}
